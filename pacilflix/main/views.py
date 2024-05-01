f45536a37bd8469244201be39482b9c94793d4c3
novitaaams <novitaaams@gmail.com>
2024-05-01 15:16:30 +0700

    udah semua ya harusnya

--- pacilflix/main/views.py
+++ pacilflix/main/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'name': 'Pak Bepe',
+        'class': 'PBP A'
+    }
+
+    return render(request, "main.html", context)
+
+
+def show_trailer(request):
+
+    return render(request, "trailer.html")
+
+
+def show_tayangan(request):
+
+    return render(request, "tayangan.html")
+
+def show_ulasan(request):
+
+    return render(request, "ulasan.html")
+
+def show_series(request):
+
+    return render(request, "series.html")
+
+def show_film(request):
+
+    return render(request, "film.html")
+
+def show_episode(request):
+
+    return render(request, "episode.html")
