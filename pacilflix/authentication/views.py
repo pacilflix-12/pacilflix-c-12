e2bdffa4bbd1ceb2152c28ce2b6ce1dd6b40b3de
Arxeno <rmfbkw04@gmail.com>
2024-05-15 12:51:21 +0700

    delete env

--- pacilflix/authentication/views.py
+++ pacilflix/authentication/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-def auth(request):
+def auth(request: HttpRequest) -> HttpResponse:
     return render(request, 'auth.html')
 
-def login(request):
+def login(request: HttpRequest) -> HttpResponse:
     return render(request, 'login.html')
 
-def register(request):
+def register(request: HttpRequest) -> HttpResponse:
+    print(request.method)
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        negara_asal = request.POST.get('country', None)
+        print(username, password, negara_asal)
+
     return render(request, 'register.html')
