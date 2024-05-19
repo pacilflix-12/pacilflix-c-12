ca5fdd136b0e4aa7d5047c9f73f5c38ecbb8f2dd
nabilazainina <nabilazainina@gmail.com>
2024-05-19 13:41:51 +0700

    Memperbaiki Trigger untuk fitur biru

--- pacilflix/authentication/views.py
+++ pacilflix/authentication/views.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError, InternalError
 from django.shortcuts import redirect, render
 from django.http import HttpRequest, HttpResponse
 from utils.get_user import get_user
@@ -29,21 +30,35 @@ def login(request: HttpRequest) -> HttpResponse:
     return render(request, 'login.html')
 
 
+
 def register(request: HttpRequest) -> HttpResponse:
+    error_message = None
     if request.method == 'POST':
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)
         country = request.POST.get('country', None)
+        
+        try:
+            insert(
+                f"INSERT INTO PENGGUNA (username, password, negara_asal) VALUES ('{username}', '{password}', '{country}')")
+            response = redirect('/tayangan/')
+            # Set cookie for the ticket on login
+            response.set_cookie('ticket', username)
+            return response
+        except IntegrityError as e:
+            # Check if the exception is due to the custom trigger
+            if 'username' in str(e).lower():
+                error_message = 'Error: Username already exists'
+            else:
+                error_message = 'An unexpected error occurred'
+        except InternalError as e:
+            # Handle InternalError separately if necessary
+            if 'username' in str(e).lower():
+                error_message = 'Error: Username already exists'
+            else:
+                error_message = 'An unexpected error occurred'
 
-        insert(
-            f"INSERT INTO PENGGUNA (username, password, negara_asal) VALUES ('{username}', '{password}', '{country}')")
-
-        response = redirect('/tayangan/')
-        # set cookie untuk ticket setiap login
-        response.set_cookie('ticket', username)
-        return response
-
-    return render(request, 'register.html')
+    return render(request, 'register.html', {'error': error_message, 'islogin': request.user.is_authenticated})
 
 
 def logout(request: HttpRequest) -> HttpResponse:
