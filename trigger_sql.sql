eae24d75623eb5361784d015d2b8a4ae54154deb
dindakiranaa <dindakirana104@gmail.com>
2024-05-19 14:07:24 +0700

    Add trigger kuning

--- trigger_sql.sql
+++ trigger_sql.sql
@@ -7,3 +7,18 @@ BEGIN
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
+-- trigger kuning
+CREATE OR REPLACE FUNCTION restrict_recent_download_deletion () RETURNS TRIGGER AS $$
+BEGIN
+    IF OLD.timestamp BETWEEN (CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Jakarta') - INTERVAL '24 hours' AND
+                               CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Jakarta' THEN
+        RAISE EXCEPTION 'Shows downloaded less than a day ago cannot be deleted';
+    ELSE
+        RETURN OLD;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER restrict_recent_download_deletion_trigger BEFORE DELETE ON TAYANGAN_TERUNDUH FOR EACH ROW
+EXECUTE FUNCTION restrict_recent_download_deletion ();
